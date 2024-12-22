1c8873acbb418b783eea14499ba7cbc0972990a4
SIDDHESH <118993933+Sid4510@users.noreply.github.com>
2024-12-22 12:30:05 +0530

    Updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        headline: ['Merriweather', 'serif'], // For titles and headlines
+        body: ['Roboto', 'sans-serif'], // For body text
+      },
+      
       colors: {
         nav: "#404044",
+        background: "#f8f9fa",
+        text: "#212529",
+
       },
     },
   },
